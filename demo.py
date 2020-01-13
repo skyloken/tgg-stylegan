700ccd721c4722cbd0e7f085811ceab76246e83b
skyloken <risora1234@gmail.com>
2020-01-14 03:03:01 +0900

    Fix demo app

--- demo.py
+++ demo.py
@@ -33,18 +33,25 @@ class Application(tk.Frame):
         network_pkl = misc.locate_network_pkl(run_id, snapshot)
         print('Loading networks from "%s"...' % network_pkl)
         _G, _D, self.Gs = misc.load_pkl(network_pkl)
+    
+    def get_label(self):
+        western = 1 if self.is_western.get() else 0
+        japanese = 1 if self.is_japanese.get() else 0
+        return [western, japanese]
 
     def generate_figure(self):
-        label = np.identity(2)[self.label_var.get()]
+        label = self.get_label()
         rnd = np.random.RandomState(None)
         self.latents = rnd.randn(1, self.Gs.input_shape[1])
-        self.dlatents = self.Gs.components.mapping.run(self.latents, [label])
-        images = self.Gs.run(self.latents, [label], truncation_psi=self.truncation_psi_var.get(), randomize_noise=True,
-                             output_transform=fmt)
+        dlatents = self.Gs.components.mapping.run(self.latents, [label])
+        dlatent_avg = self.Gs.get_var('dlatent_avg')
+        self.dlatents = (dlatents - dlatent_avg) * np.reshape(self.truncation_psi_var.get(), [-1, 1, 1]) + dlatent_avg
+
+        images = self.Gs.run(self.latents, [label], truncation_psi=self.truncation_psi_var.get(), randomize_noise=False, output_transform=fmt)
         return ImageTk.PhotoImage(image=Image.fromarray(images[0], 'RGB'))
 
     def generate_transition_animation(self):
-        label = np.identity(2)[self.label_var.get()]
+        label = self.get_label()
         images = []
         num_split = 100
         rnd = np.random.RandomState(None)
@@ -57,18 +64,26 @@ class Application(tk.Frame):
             images.append(ImageTk.PhotoImage(image=Image.fromarray(images_out[0], 'RGB')))
 
         self.latents = dst_latents
-        self.dlatents = self.Gs.components.mapping.run(self.latents, [label])
+        dlatents = self.Gs.components.mapping.run(self.latents, [label])
+        dlatent_avg = self.Gs.get_var('dlatent_avg')
+        self.dlatents = (dlatents - dlatent_avg) * np.reshape(self.truncation_psi_var.get(), [-1, 1, 1]) + dlatent_avg
 
         return images
 
-    def generate_color_changed_figure(self):
+    def generate_style_changed_figure(self):
 
-        label = np.identity(2)[self.label_var.get()]
+        label = self.get_label()
         rnd = np.random.RandomState(None)
+        
         dst_latents = rnd.randn(1, self.Gs.input_shape[1])
         dst_dlatents = self.Gs.components.mapping.run(dst_latents, [label])
 
-        self.dlatents[:, 13] = dst_dlatents[:, 13]
+        if self.style_var.get() == 0:
+            self.dlatents[:, 4:8] = dst_dlatents[:, 4:8]
+        elif self.style_var.get() == 1:
+            self.dlatents[:, 8:14] = dst_dlatents[:, 8:14]
+        elif self.style_var.get() == 2:
+            self.dlatents[:, 13] = dst_dlatents[:, 13]
         images = self.Gs.components.synthesis.run(self.dlatents, truncation_psi=self.truncation_psi_var.get(),
                                                   randomize_noise=False, output_transform=fmt)
         return ImageTk.PhotoImage(image=Image.fromarray(images[0], 'RGB'))
@@ -77,12 +92,16 @@ class Application(tk.Frame):
         global img
 
         # values
-        self.label_var = tk.IntVar()
-        self.label_var.set(0)
+        self.is_western = tk.BooleanVar()
+        self.is_western.set(True)
+        self.is_japanese = tk.BooleanVar()
+        self.is_japanese.set(False)
         self.is_animation = tk.BooleanVar()
         self.is_animation.set(False)
-        self.truncation_psi_var = tk.IntVar()
-        self.truncation_psi_var.set(0.7)
+        self.truncation_psi_var = tk.DoubleVar()
+        self.truncation_psi_var.set(1.0)
+        self.style_var = tk.IntVar()
+        self.style_var.set(2)
 
         # Figure canvas
         img = self.generate_figure()
@@ -90,28 +109,36 @@ class Application(tk.Frame):
         self.image_on_canvas = self.canvas.create_image(0, 0, anchor='nw', image=img)
 
         # Label button
-        self.western_radio_button = tk.Radiobutton(self, value=0, variable=self.label_var, text='Western')
-        self.japanese_radio_button = tk.Radiobutton(self, value=1, variable=self.label_var, text='Japanese')
+        self.western_checkbox = tk.Checkbutton(self, text='Western', variable=self.is_western)
+        self.japanese_checkbox = tk.Checkbutton(self, text='Japanese', variable=self.is_japanese)
+
+        # Style button
+        self.middle_radio_button = tk.Radiobutton(self, value=0, variable=self.style_var, text='Middle')
+        self.fine_radio_button = tk.Radiobutton(self, value=1, variable=self.style_var, text='Fine')
+        self.color_radio_button = tk.Radiobutton(self, value=2, variable=self.style_var, text='Color')
 
         # Settings
-        self.animation_checkbox = tk.Checkbutton(text='Animation', variable=self.is_animation)
-        self.truncation_psi_scale = tk.Scale(self, orient="horizontal", variable=self.truncation_psi_var)
+        self.animation_checkbox = tk.Checkbutton(self, text='Animation', variable=self.is_animation)
+        self.truncation_psi_scale = tk.Scale(self, orient="horizontal", variable=self.truncation_psi_var, from_=0.0, to=1.0, resolution=0.1)
 
         # Generate button
         self.generate_button = tk.Button(self, text='Generate', command=self.generate)
-        self.change_color_button = tk.Button(self, text='Change color', command=self.change_color)
+        self.change_style_button = tk.Button(self, text='Change style', command=self.change_style)
 
         # Quit button
         self.quit_button = tk.Button(self, text='Quit', fg='red', command=self.master.destroy)
 
         # Pack
         self.canvas.pack()
-        self.western_radio_button.pack()
-        self.japanese_radio_button.pack()
+        self.western_checkbox.pack()
+        self.japanese_checkbox.pack()
         self.animation_checkbox.pack()
         self.truncation_psi_scale.pack()
         self.generate_button.pack()
-        self.change_color_button.pack()
+        self.middle_radio_button.pack()
+        self.fine_radio_button.pack()
+        self.color_radio_button.pack()
+        self.change_style_button.pack()
         self.quit_button.pack()
 
     def generate(self):
@@ -128,16 +155,16 @@ class Application(tk.Frame):
                 self.canvas.update()
                 time.sleep(0.05)
 
-            self.generate_button['text'] = 'Transition'
+            self.generate_button['text'] = 'Generate'
             self.generate_button['state'] = 'normal'
         else:
             img = self.generate_figure()
             self.canvas.itemconfig(self.image_on_canvas, image=img)
 
-    def change_color(self):
+    def change_style(self):
         global img
 
-        img = self.generate_color_changed_figure()
+        img = self.generate_style_changed_figure()
         self.canvas.itemconfig(self.image_on_canvas, image=img)
 
 
