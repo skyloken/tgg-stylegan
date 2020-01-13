f736119d977fa18dfe8f085df02b40f03f0042b5
Kenta Tanigaki <risora1234@gmail.com>
2020-01-14 01:41:09 +0900

    Add change color button

--- demo.py
+++ demo.py
@@ -35,14 +35,16 @@ class Application(tk.Frame):
         _G, _D, self.Gs = misc.load_pkl(network_pkl)
 
     def generate_figure(self):
-        label = np.identity(2)[self.var.get()]
+        label = np.identity(2)[self.label_var.get()]
         rnd = np.random.RandomState(None)
         self.latents = rnd.randn(1, self.Gs.input_shape[1])
-        images = self.Gs.run(self.latents, [label], truncation_psi=1.0, randomize_noise=True, output_transform=fmt)
+        self.dlatents = self.Gs.components.mapping.run(self.latents, [label])
+        images = self.Gs.run(self.latents, [label], truncation_psi=self.truncation_psi_var.get(), randomize_noise=True,
+                             output_transform=fmt)
         return ImageTk.PhotoImage(image=Image.fromarray(images[0], 'RGB'))
 
     def generate_transition_animation(self):
-        label = np.identity(2)[self.var.get()]
+        label = np.identity(2)[self.label_var.get()]
         images = []
         num_split = 100
         rnd = np.random.RandomState(None)
@@ -50,67 +52,96 @@ class Application(tk.Frame):
 
         for i in range(num_split + 1):
             latents = self.latents + (dst_latents - self.latents) * i / num_split
-            images_out = self.Gs.run(latents, [label], truncation_psi=1.0, randomize_noise=False, output_transform=fmt)
+            images_out = self.Gs.run(latents, [label], truncation_psi=self.truncation_psi_var.get(),
+                                     randomize_noise=False, output_transform=fmt)
             images.append(ImageTk.PhotoImage(image=Image.fromarray(images_out[0], 'RGB')))
+
         self.latents = dst_latents
+        self.dlatents = self.Gs.components.mapping.run(self.latents, [label])
 
         return images
 
+    def generate_color_changed_figure(self):
+
+        label = np.identity(2)[self.label_var.get()]
+        rnd = np.random.RandomState(None)
+        dst_latents = rnd.randn(1, self.Gs.input_shape[1])
+        dst_dlatents = self.Gs.components.mapping.run(dst_latents, [label])
+
+        self.dlatents[:, 13] = dst_dlatents[:, 13]
+        images = self.Gs.components.synthesis.run(self.dlatents, truncation_psi=self.truncation_psi_var.get(),
+                                                  randomize_noise=False, output_transform=fmt)
+        return ImageTk.PhotoImage(image=Image.fromarray(images[0], 'RGB'))
+
     def create_widgets(self):
         global img
-        self.var = tk.IntVar()
-        self.var.set(0)
+
+        # values
+        self.label_var = tk.IntVar()
+        self.label_var.set(0)
+        self.is_animation = tk.BooleanVar()
+        self.is_animation.set(False)
+        self.truncation_psi_var = tk.IntVar()
+        self.truncation_psi_var.set(0.7)
 
         # Figure canvas
         img = self.generate_figure()
-        # img = Image.new('RGB', (256, 256), 0)
         self.canvas = tk.Canvas(self, width=width, height=height)
         self.image_on_canvas = self.canvas.create_image(0, 0, anchor='nw', image=img)
 
         # Label button
-        self.radio_button1 = tk.Radiobutton(self, value=0, variable=self.var, text='Western')
-        self.radio_button2 = tk.Radiobutton(self, value=1, variable=self.var, text='Japanese')
+        self.western_radio_button = tk.Radiobutton(self, value=0, variable=self.label_var, text='Western')
+        self.japanese_radio_button = tk.Radiobutton(self, value=1, variable=self.label_var, text='Japanese')
+
+        # Settings
+        self.animation_checkbox = tk.Checkbutton(text='Animation', variable=self.is_animation)
+        self.truncation_psi_scale = tk.Scale(self, orient="horizontal", variable=self.truncation_psi_var)
 
         # Generate button
         self.generate_button = tk.Button(self, text='Generate', command=self.generate)
-        self.transition_button = tk.Button(self, text='Transition', command=self.transition)
+        self.change_color_button = tk.Button(self, text='Change color', command=self.change_color)
 
         # Quit button
         self.quit_button = tk.Button(self, text='Quit', fg='red', command=self.master.destroy)
 
         # Pack
         self.canvas.pack()
-        self.radio_button1.pack()
-        self.radio_button2.pack()
+        self.western_radio_button.pack()
+        self.japanese_radio_button.pack()
+        self.animation_checkbox.pack()
+        self.truncation_psi_scale.pack()
         self.generate_button.pack()
-        self.transition_button.pack()
+        self.change_color_button.pack()
         self.quit_button.pack()
 
     def generate(self):
         global img
-        img = self.generate_figure()
+
+        if self.is_animation.get():
+            self.generate_button['text'] = 'Generating...'
+            self.generate_button['state'] = 'disable'
+
+            images = self.generate_transition_animation()
+            for image in images:
+                img = image
+                self.canvas.itemconfig(self.image_on_canvas, image=img)
+                self.canvas.update()
+                time.sleep(0.05)
+
+            self.generate_button['text'] = 'Transition'
+            self.generate_button['state'] = 'normal'
+        else:
+            img = self.generate_figure()
+            self.canvas.itemconfig(self.image_on_canvas, image=img)
+
+    def change_color(self):
+        global img
+
+        img = self.generate_color_changed_figure()
         self.canvas.itemconfig(self.image_on_canvas, image=img)
 
-    def transition(self):
-        global img
-        self.transition_button['text'] = 'Generating...'
-        self.transition_button['state'] = 'disable'
-
-        images = self.generate_transition_animation()
-        for image in images:
-            img = image
-            self.canvas.itemconfig(self.image_on_canvas, image=img)
-            self.canvas.update()
-            time.sleep(0.05)
-
-        self.transition_button['text'] = 'Transition' 
-        self.transition_button['state'] = 'normal'
-
-import tkinter.font as font
 
 if __name__ == '__main__':
     root = tk.Tk()
-    root.option_add('*font', 'System 14')
-    print(font.families())
     app = Application(master=root)
     app.mainloop()
