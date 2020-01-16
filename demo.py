7d5e5a75f7cce99d783a150d11d3137ddcf2d848
skyloken <risora1234@gmail.com>
2020-01-17 00:05:14 +0900

    Fix demo app

--- demo.py
+++ demo.py
@@ -13,8 +13,8 @@ import time
 seed = 100
 run_id = 0
 snapshot = None
-width = 256
-height = 256
+width = 512
+height = 512
 fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
 global img
 
@@ -43,12 +43,12 @@ class Application(tk.Frame):
         label = self.get_label()
         rnd = np.random.RandomState(None)
         self.latents = rnd.randn(1, self.Gs.input_shape[1])
-        dlatents = self.Gs.components.mapping.run(self.latents, [label])
-        dlatent_avg = self.Gs.get_var('dlatent_avg')
-        self.dlatents = (dlatents - dlatent_avg) * np.reshape(self.truncation_psi_var.get(), [-1, 1, 1]) + dlatent_avg
-
-        images = self.Gs.run(self.latents, [label], truncation_psi=self.truncation_psi_var.get(), randomize_noise=False, output_transform=fmt)
-        return ImageTk.PhotoImage(image=Image.fromarray(images[0], 'RGB'))
+        self.dlatents = self.Gs.components.mapping.run(self.latents, [label])
+        if not self.is_validation.get():
+            dlatent_avg = self.Gs.get_var('dlatent_avg')
+            self.dlatents = (self.dlatents - dlatent_avg) * np.reshape([self.truncation_psi_var.get()], [-1, 1, 1]) + dlatent_avg
+        images = self.Gs.run(self.latents, [label], truncation_psi=self.truncation_psi_var.get(), truncation_cutoff=15, is_validation=self.is_validation.get(), randomize_noise=False, output_transform=fmt)
+        return ImageTk.PhotoImage(image=Image.fromarray(images[0], 'RGB').resize((512, 512)))
 
     def generate_transition_animation(self):
         label = self.get_label()
@@ -59,14 +59,15 @@ class Application(tk.Frame):
 
         for i in range(num_split + 1):
             latents = self.latents + (dst_latents - self.latents) * i / num_split
-            images_out = self.Gs.run(latents, [label], truncation_psi=self.truncation_psi_var.get(),
-                                     randomize_noise=False, output_transform=fmt)
-            images.append(ImageTk.PhotoImage(image=Image.fromarray(images_out[0], 'RGB')))
+            images_out = self.Gs.run(latents, [label], truncation_psi=self.truncation_psi_var.get(), truncation_cutoff=15,
+                                     is_validation=self.is_validation.get(), randomize_noise=False, output_transform=fmt)
+            images.append(ImageTk.PhotoImage(image=Image.fromarray(images_out[0], 'RGB').resize((512, 512))))
 
         self.latents = dst_latents
-        dlatents = self.Gs.components.mapping.run(self.latents, [label])
-        dlatent_avg = self.Gs.get_var('dlatent_avg')
-        self.dlatents = (dlatents - dlatent_avg) * np.reshape(self.truncation_psi_var.get(), [-1, 1, 1]) + dlatent_avg
+        self.dlatents = self.Gs.components.mapping.run(self.latents, [label])
+        if not self.is_validation.get():
+            dlatent_avg = self.Gs.get_var('dlatent_avg')
+            self.dlatents = (self.dlatents - dlatent_avg) * np.reshape([self.truncation_psi_var.get()], [-1, 1, 1]) + dlatent_avg
 
         return images
 
@@ -83,10 +84,11 @@ class Application(tk.Frame):
         elif self.style_var.get() == 1:
             self.dlatents[:, 8:14] = dst_dlatents[:, 8:14]
         elif self.style_var.get() == 2:
-            self.dlatents[:, 13] = dst_dlatents[:, 13]
-        images = self.Gs.components.synthesis.run(self.dlatents, truncation_psi=self.truncation_psi_var.get(),
-                                                  randomize_noise=False, output_transform=fmt)
-        return ImageTk.PhotoImage(image=Image.fromarray(images[0], 'RGB'))
+            self.dlatents[:, 0:13] = dst_dlatents[:, 0:13]
+        elif self.style_var.get() == 3:
+            self.dlatents[:, 13:14] = dst_dlatents[:, 13:14]
+        images = self.Gs.components.synthesis.run(self.dlatents, randomize_noise=False, output_transform=fmt)
+        return ImageTk.PhotoImage(image=Image.fromarray(images[0], 'RGB').resize((512, 512)))
 
     def create_widgets(self):
         global img
@@ -96,12 +98,14 @@ class Application(tk.Frame):
         self.is_western.set(True)
         self.is_japanese = tk.BooleanVar()
         self.is_japanese.set(False)
+        self.is_validation = tk.BooleanVar()
+        self.is_validation.set(False)
         self.is_animation = tk.BooleanVar()
         self.is_animation.set(False)
         self.truncation_psi_var = tk.DoubleVar()
-        self.truncation_psi_var.set(1.0)
+        self.truncation_psi_var.set(0.7)
         self.style_var = tk.IntVar()
-        self.style_var.set(2)
+        self.style_var.set(3)
 
         # Figure canvas
         img = self.generate_figure()
@@ -115,11 +119,13 @@ class Application(tk.Frame):
         # Style button
         self.middle_radio_button = tk.Radiobutton(self, value=0, variable=self.style_var, text='Middle')
         self.fine_radio_button = tk.Radiobutton(self, value=1, variable=self.style_var, text='Fine')
-        self.color_radio_button = tk.Radiobutton(self, value=2, variable=self.style_var, text='Color')
+        self.pattern_radio_button = tk.Radiobutton(self, value=2, variable=self.style_var, text='Pattern')
+        self.color_radio_button = tk.Radiobutton(self, value=3, variable=self.style_var, text='Color')
 
         # Settings
+        self.validation_checkbox = tk.Checkbutton(self, text='Validation', variable=self.is_validation)
         self.animation_checkbox = tk.Checkbutton(self, text='Animation', variable=self.is_animation)
-        self.truncation_psi_scale = tk.Scale(self, orient="horizontal", variable=self.truncation_psi_var, from_=0.0, to=1.0, resolution=0.1)
+        self.truncation_psi_scale = tk.Scale(self, orient="horizontal", variable=self.truncation_psi_var, length=200, from_=-1.0, to=1.0, resolution=0.1)
 
         # Generate button
         self.generate_button = tk.Button(self, text='Generate', command=self.generate)
@@ -132,11 +138,13 @@ class Application(tk.Frame):
         self.canvas.pack()
         self.western_checkbox.pack()
         self.japanese_checkbox.pack()
+        self.validation_checkbox.pack()
         self.animation_checkbox.pack()
         self.truncation_psi_scale.pack()
         self.generate_button.pack()
         self.middle_radio_button.pack()
         self.fine_radio_button.pack()
+        self.pattern_radio_button.pack()
         self.color_radio_button.pack()
         self.change_style_button.pack()
         self.quit_button.pack()
